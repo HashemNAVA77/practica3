0acd74697f92e5f21fe430cfd5511a6d554c1f42
HashemNAVA77 <143622962+HashemNAVA77@users.noreply.github.com>
2023-09-08 08:54:49 -0600

    Add files via upload

---
+++ practica3hnb.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.operaciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class practicatraingulohnb {
+    public static void  main (String [] args ){
+        //numeros enteros
+        //numeros decimales
+         int Base=5;
+         int Altura=10;
+         int resultado =Base * Altura/2;
+         
+         int multiplicacion = Base * Altura /2;
+         
+         
+         
+                
+    }
+    
+}
